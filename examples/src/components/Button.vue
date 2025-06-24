85f7555b8486ce5c4acad5b9d9d59452bfbcb267
杨静 <1369374442@qq.com>
2025-06-24 16:37:13 +0800

    feat: 开发第一个组件button，并使用workspace方式链接本地包core到examples项目中

---
+++ examples/src/components/Button.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { Button } from '@wli-admin-components/core'
+</script>
+
+<template>
+  <Button>Click me</Button>
+</template>
+
+<style scoped lang="scss"></style>
---
+++ packages/core/Button/src/Button.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <button>Click me</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss"></style>
